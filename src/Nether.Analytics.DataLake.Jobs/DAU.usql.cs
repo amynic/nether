29f2502fffd6c2f2ecb711cc364188c1a372f601
Amy Kate Nicholson <amykate_7@live.com>
2017-05-17 11:05:19 -0700

    DAU and MAU scripts on new branch

---
+++ src/Nether.Analytics.DataLake.Jobs/DAU.usql.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Nether.Analytics.DataLake.Jobs
+{
+    public class eventDate
+    {
+        public static DateTime EventDateValue { get; set; }
+
+        public static DateTime convertDate(string eventDate)
+        {
+            DateTime newdate = DateTime.Parse(eventDate);
+            return newdate;
+        }
+        
+    }
+}
